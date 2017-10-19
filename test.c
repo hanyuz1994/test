032dabd0ea55d550527354542631fedc2ad7ad3a
zhanghanyu <zhanghanyu@bbktel.com>
2017-10-19 21:24:54 +0800

    在英文歌词基础上增加中文歌词

---
+++ test.c
@@ -0,0 +1,55 @@
+I spent half year’s savings sailing to see you
+For which
+I practiced every breath repeatedly
+Not a word could carry my feelings about you
+I tossed and turned at night thinking about you
+Memory is the thing that cannot be erased easily
+For what you said
+I held tears in my eyes with desperation
+At the same corner in an unacquainted city
+We once comforted each other
+and we signed in each other’s arms
+For whatever the result was
+I saw you off in a windy day
+Couldn’t help weeping sorrowfully
+How I expectd to be accompanying you to the end of my life
+为你我用了半年的积蓄漂洋过海的来看你
+为了这次相聚
+我连见面时的呼吸都曾反复练习
+言语从来没能将我的情意表达千万分之一
+为了这个遗憾我在夜里想了又想不肯睡去
+记忆它总是慢慢的累积在我心中无法抹去
+为了你的承诺
+我在最绝望的时候都忍着不哭泣
+陌生的城市啊熟悉的角落里
+也曾彼此安慰也曾相拥叹息
+不管将要面对什么样的结局
+在漫天风沙里望着你远去
+我竟悲伤得不能自己
+多盼能送君千里直到山穷水尽一生和你相依
+为你我用了半年的积蓄漂洋过海的来看你
+为了这次相聚
+我连见面时的呼吸都曾反复练习
+言语从来没能将我的情意表达千万分之一
+为了这个遗憾我在夜里想了又想不肯睡去
+记忆它总是慢慢的累积
+在我心中无法抹去为了你的承诺
+我在最绝望的时候都忍着不哭泣
+陌生的城市啊熟悉的角落里
+也曾彼此安慰也曾相拥叹息
+不管将要面对什么样的结局
+在漫天风沙里望着你远去
+我竟悲伤得不能自己
+多盼能送君千里直到山穷水尽一生和你相依
+陌生的城市啊熟悉的角落里
+也曾彼此安慰也曾相拥叹息
+不管将要面对什么样的结局
+在漫天风沙里望着你远去
+我竟悲伤得不能自己
+多盼能送君千里直到山穷水尽一生和你相依
+陌生的城市啊熟悉的角落里
+也曾彼此安慰也曾相拥叹息
+不管将要面对什么样的结局
+在漫天风沙里望着你远去
+我竟悲伤得不能自己
+多盼能送君千里直到山穷水尽一生和你相依
